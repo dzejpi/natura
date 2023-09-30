b5553b4dac308d416f7ce1850931d2c380c5daac
DzejPi <dzejjpii@gmail.com>
2023-09-30 10:54:25 +0200

    Created a very simple map, split into chunks for better performance, wrote a script to generate collisions on the terrain.

---
+++ scripts/game_scenes/WorldTerrain.gd
@@ -0,0 +1,11 @@
+extends Spatial
+
+
+# Generate collision for map chunks
+func _ready():
+	var children_node_count = get_child_count()
+	
+	if children_node_count > 0:
+		for i in children_node_count:
+			get_child(i).get_child(0).create_trimesh_collision()
+			i += 1
