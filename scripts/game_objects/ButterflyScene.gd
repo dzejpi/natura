8a748310b838bc9443bf12db8169e8aa15bcd0e9
DzejPi <dzejjpii@gmail.com>
2023-10-01 16:34:29 +0200

    Butterflies go into the shelter in winter.

--- scripts/game_objects/ButterflyScene.gd
+++ scripts/game_objects/ButterflyScene.gd
@@ -21,15 +21,14 @@ var current_season = 0
 
 func _ready():
 	current_season = season_management_node.current_season
-	check_for_winter()
 	butterfly_animation_player.play("fly")
 
 
 func _process(delta):
-	check_for_winter()
+	check_for_winter(delta)
 	
 	if is_winter:
-		fly_towards_tree(delta)
+		fly_towards_shelter(delta)
 	else:
 		fly_towards_tree(delta)
 
@@ -73,17 +72,41 @@ func fly_towards_tree(delta):
 				butterfly_countdown = butterfly_countdown_value
 				
 				if current_target:
-					current_target.pollen_left -= 1
+					if current_target.pollen_left:
+						current_target.pollen_left -= 1
 					is_closest_tree_found = false
 		else:
 			look_at(current_target.global_transform.origin, Vector3(0, 1, 0))
 			translate(Vector3(0, 0, -butterfly_speed * delta))
 
 
-func check_for_winter():
+func fly_towards_shelter(delta):
+	if !is_closest_shelter_found:
+		find_closest_shelter()
+	else:
+		var direction = current_target.global_transform.origin - global_transform.origin
+		
+		# Move the bee
+		if direction.length() < 0.2:
+			if butterfly_countdown > 0:
+				butterfly_countdown -= 1 * delta
+			else:
+				butterfly_countdown = butterfly_countdown_value
+				
+				if current_target:
+					is_closest_shelter_found = false
+		else:
+			look_at(current_target.global_transform.origin, Vector3(0, 1, 0))
+			translate(Vector3(0, 0, -butterfly_speed * delta))
+
+
+func check_for_winter(delta):
 	if current_season != season_management_node.current_season:
 		current_season = season_management_node.current_season
 		if current_season == 3:
 			is_winter = true
+			is_closest_tree_found = false
+			is_closest_shelter_found = false
 		else:
 			is_winter = false
+			is_closest_shelter_found = false
